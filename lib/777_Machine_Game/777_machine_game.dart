5735f0ed6a0501fafcbea56533adaf6654972685
satyapsr13 <satyapsr13@gmail.com>
2023-05-08 10:22:30 +0530

    777 machinge game

---
+++ lib/777_Machine_Game/777_machine_game.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class Machine777Game extends StatefulWidget {
+  const Machine777Game({super.key});
+
+  @override
+  State<Machine777Game> createState() => _Machine777GameState();
+}
+
+class _Machine777GameState extends State<Machine777Game> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('777 Machine Game'),
+      ),
+      body: Column(
+        children: [],
+      ),
+      //floatingActionButton: FloatingActionButton(onPressed: (){},),
+    );
+  }
+}
